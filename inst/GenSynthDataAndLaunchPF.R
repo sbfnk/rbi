b1b73be6b4ccd79b126297c0f1c039c1e1a43ee2
Pierre Jacob <pierre.jacob.work@gmail.com>
2013-03-21 10:23:39 +0000

    added example how to create an init file using bi_create_parameters

---
+++ inst/GenSynthDataAndLaunchPF.R
@@ -0,0 +1,38 @@
+### This doesn't work because the PZ model is broken (not reading init.nc properly)
+
+rm(list = ls(all.names=TRUE))
+unlink(".RData")
+try(detach(package:bi, unload = TRUE), silent = TRUE)
+library(bi, quietly = TRUE)
+
+pathToModel <- "/home/pierre/workspace/pz/"
+pathToBi <- "/home/pierre/workspace/bi/script/"
+modelFile <- "PZ.bi"
+initfile <- "/tmp/someinitfile.nc"
+TimeHorizon <- 1000
+parameters <- list(EPg = 0.3, VPg = 0.1)
+
+bi_create_parameters(initfile, parameters)
+
+# Settings
+fsettings <- bi::settings(mode = "filter", configfile = "filter.conf",
+                         pathModel = pathToModel,
+                         pathBi = pathToBi, 
+                         args = paste("--init-file", initfile))
+
+obsfile <- tempfile(pattern = "obs", fileext = ".nc")
+gensettings <- bi::settings(mode = "simulate",
+                            args = paste("--model-file", modelFile,
+                            "--seed ", 123, "--init-file", initfile), pathModel = pathToModel, 
+                            pathBi = pathToBi)
+invar <- "P"; outvar <- "P_obs"
+S <- c(0.1); logn <- TRUE
+
+bi::gen_obs(gensettings, TimeHorizon, TimeHorizon, invar, outvar, obsfile, S, logn)
+
+print(settings)
+verbose = FALSE
+# Once happy with the settings, launch bi.
+fargs <- paste0("-T ", TimeHorizon, " --obs-file ", obsfile, 
+               " -P 256 --output-file results/launchPZ_PF.nc --verbose --threads 1" )
+bi::launcher(fsettings, args=fargs)
