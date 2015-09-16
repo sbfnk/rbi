238130de4a5e65d28c2bb284a7c3803b7f323a69
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-09-16 16:15:39 -0400

    dim_len

---
+++ R/bi_dim_len.R
@@ -0,0 +1,15 @@
+#' @rdname bi_dim_len
+#' @name bi_dim_len
+#' @title NetCDF dimension length
+#' @description
+#' This function returns the length of a dimension in a NetCDF file.
+#' @param filename path to a NetCDF file
+#' @return dimension length
+#' @export
+#' 
+bi_dim_len <- function(filename, dim){
+  ncfile <-  open.ncdf(tools::file_path_as_absolute(filename), verbose = FALSE)  
+  len <- ncfile$dim[[which(names(ncfile$dim) == dim)]]$len
+  close.ncdf(ncfile)
+  return(len)
+}
