5a6febf9ac3def2e386c2c55cbe13492ebc16437
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-09-16 16:19:05 -0400

    option_string: any number of options

--- R/util_option_string.R
+++ R/util_option_string.R
@@ -4,29 +4,37 @@
 #' @description
 #' This function is used to convert a list of options into an options string. If a string is given,  it will be taken as such.
 #'
-#' @param options list of options, or a string (which will be returned unmodified)
+#' @param ... any number of lists of options, or strings (which will be left unmodified). If lists are given, later arguments will override earlier ones
 #
-option_string <- function(options){
-  if (is.character(options)) {
-    string <- option
-  } else if (is.list(options)) {
-    string <-
-      paste(sapply(names(options),
-                   function(option) {
-                     if (is.logical(options[[option]])) {
-                       if (options[[option]] == TRUE)
-                         paste0("--enable-", option)
-                       else
-                         paste0("--disable-", option)
-                     } else {
-                       paste0("--", option, " ",
-                              format(options[[option]], scientifi = FALSE))
-                     }
-                   }
-                   ), collapse = " ")
-  } else {
-    string <- ""
+option_string <- function(...){
+  list_options <- list()
+  string <- ""
+
+  for (option in list(...)) {
+    if (is.character(option)) {
+      string <- paste(string, option)
+    } else if (is.list(option)) {
+      list_options[names(option)] <- option
+    } else {
+      stop("arguments to 'option_string' must be lists or character vectors")
+    }
   }
-    
-  return(string)
+
+  string <- paste(paste(sapply(names(list_options),
+                         function(option) {
+                           if (is.logical(list_options[[option]])) {
+                             if (list_options[[option]] == TRUE)
+                               paste0("--enable-", option)
+                             else
+                               paste0("--disable-", option)
+                           } else {
+                             paste0("--", option, " ",
+                                    format(list_options[[option]],
+                                           scientific = FALSE))
+                           }
+                         }
+                         ), collapse = " "), string)
+
+
+return(string)
 }
