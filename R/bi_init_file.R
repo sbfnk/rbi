91326a4480c32b8eff4f978600a651444fa4724a
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-09-22 00:53:40 +0100

    bi_init_file: allow more variable types

--- R/bi_init_file.R
+++ R/bi_init_file.R
@@ -9,8 +9,7 @@
 #' 
 #' @param filename a path to a NetCDF file to write the variables into, which will be overwritten
 #' if it already exists.
-#' @param variables a \code{list} object, which names should be the variable names and values should
-#' be vectors of equal length (or simply one value per key)
+#' @param variables a \code{list} object, which names should be the variable names and values should be either single values, vectors of equal length, or data frames
 #' @return None, but creates a NetCDF file at the specified path.
 #' @export
 bi_init_file <- function(filename, variables){
@@ -18,12 +17,14 @@ bi_init_file <- function(filename, variables){
   if (length(variables) == 0){
     stop("please provide a non-empty list to bi_init_file")
   }
-  variablelengths <- as.numeric(unlist(lapply(X=variables, FUN=length)))
+  vector_variables <-
+    variables[sapply(variables, function(x) {is.numeric(x) && length(x) > 1})]
+  variablelengths <- as.numeric(unlist(lapply(X=vector_variables, FUN=length)))
   if (!all(variablelengths == variablelengths[1])){
     stop("please provide to bi_init_file a list of vectors of equal length")
   }
-  variables_with_dim <- list()
-  for (name in names(variables)){
+  variables_with_dim <- variables
+  for (name in names(vector_variables)){
     variables_with_dim[[name]] <- list(values = variables[[name]], dimension = "ns")
   }
   netcdf_create_from_list(filename, variables_with_dim)
