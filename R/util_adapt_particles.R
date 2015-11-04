6b3a7358016469fc80805a2b720b42e666901725
sbfnk <sbfnk@users.noreply.github.com>
2015-11-04 18:59:16 +0000

    work out scale yourself

--- R/util_adapt_particles.R
+++ R/util_adapt_particles.R
@@ -10,15 +10,13 @@
 #' @param init initial number of particles (default: 1)
 #' @param min minimum acceptance rate
 #' @param max maximum acceptance rate
-#' @param scale scale multiplier/divider for the number of particles (default: 2).
-#'   If >1 this will be inverted and rounded up.
 #' @param add_options list of additional options
 #' @param samples number of samples to generate each iteration
 #' @param max_iter maximum of iterations (default: 10)
 #' @param ... parameters for bi_wrapper$run
 #' @return a \code{bi_wrapper} with the desired proposal distribution
 #' @export
-adapt_particles <- function(wrapper, init = 1, min = 0, max = 1, scale = 2, add_options, samples, max_iter = 10, ...) {
+adapt_particles <- function(wrapper, init = 1, min = 0, max = 1, add_options, samples, max_iter = 10, ...) {
 
   if (missing(add_options)) {
     add_options <- list()
@@ -32,10 +30,6 @@ adapt_particles <- function(wrapper, init = 1, min = 0, max = 1, scale = 2, add_
 
   nParticles <- init
 
-  ## scale should be > 1 (it's a multiplier if acceptance rate is too
-  ## small, divider if the acceptance Rate is too big)
-  if (scale < 1) scale <- ceiling(1 / scale)
-
   model <- wrapper$model
   model$remove_block("proposal_parameter")
   model$remove_block("proposal_initial")
@@ -43,7 +37,6 @@ adapt_particles <- function(wrapper, init = 1, min = 0, max = 1, scale = 2, add_
   init_file <- wrapper$output_file_name
   init_np <- bi_dim_len(init_file, "np") - 1 ## use last parameter value from output file
 
-  accRate <- 0
   if (missing(samples)) {
     if ("nsamples" %in% names(wrapper$global_options)) {
       samples <- wrapper$global_options[["nsamples"]]
@@ -61,12 +54,12 @@ adapt_particles <- function(wrapper, init = 1, min = 0, max = 1, scale = 2, add_
   add_options[["init-file"]] <- adapt_wrapper$output_file_name
   add_options[["init-np"]] <- samples - 1
   iter <- 1
-  while ((length(accRate) == 0 | min(accRate) < min | max(accRate) > max) && iter <= max_iter && nParticles > 1) {
-    if (length(accRate) == 0 | min(accRate) < min) {
-      nParticles <- nParticles * scale
-    } else {
-      nParticles <- ceiling(nParticles / scale)
-    }
+  mcmc_obj <- mcmc(get_traces(adapt_wrapper))
+  accRate <- 1 - rejectionRate(mcmc_obj)
+  accRate <- accRate[accRate > 0]
+  
+  while ((length(accRate) == 0 | min(accRate) < min | max(accRate) > max) && iter <= max_iter && (!(max(accRate) > max && nParticles > 1))) {
+    nParticles <- 2**(round(log(1/max(accRate), 2)))
     cat("Trying ", nParticles, " particles \n")
     add_options[["nparticles"]] <- nParticles
     add_options[["init-file"]] <- adapt_wrapper$output_file_name
