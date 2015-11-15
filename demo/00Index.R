3ff6a300ff05d78e138ae37f894fa3f0152b3a01
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-11-15 16:33:55 +0100

    update demos

--- demo/00Index.R
+++ demo/00Index.R
@@ -1,5 +1,3 @@
-# import_data         Shows how to create a NetCDF file from a numeric vector in R.
-init_files          Shows how to create init files
 PZ_generate_dataset Shows how to generate data from a model using bi_generate_dataset
 PZ_filtering        Shows how to estimate filtering means with LibBi using bi_wrapper
 PZ_PMMH             Shows how to estimate the parameters with Particle Marginal Metropolis Hastings
