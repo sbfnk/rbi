403e19b621c946eb59df9c97090b3720f2ce9804
Sebastian Funk <sbfnk@users.noreply.github.com>
2016-05-04 11:04:57 +0100

    remove index file

--- demo/00Index.R
+++
@@ -1,4 +0,0 @@
-PZ_generate_dataset Shows how to generate data from a model using bi_generate_dataset
-PZ_filtering        Shows how to estimate filtering means with LibBi using bi_wrapper
-PZ_PMMH             Shows how to estimate the parameters with Particle Marginal Metropolis Hastings
-PZ_SMC2             Shows how to estimate the parameters with SMC^2
