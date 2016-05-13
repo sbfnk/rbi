e699227fda06afc74037c635426e99166300dcb7
Sebastian Funk <sbfnk@users.noreply.github.com>
2016-05-13 14:15:37 +0100

    don't warn about coords

--- R/util_nc_create_from_list.R
+++ R/util_nc_create_from_list.R
@@ -104,9 +104,6 @@ netcdf_create_from_list <- function(filename, variables, time_dim, coord_dim, va
         if (length(guessed_coord) == 1) {
           coord_dim <- guessed_coord
           index_cols <- c(index_cols, coord_dim)
-        } else
-        {
-          warning("Could not guess column from ", length(guessed_coord), " options")
         }
       }
       var_dims <- list()
