d7db429fed5ee54d533618cc82076bf57f9c6cfa
sbfnk <sbfnk@users.noreply.github.com>
2017-01-18 21:37:01 +0000

    run.libbi: don't guess noutputs

--- R/libbi.R
+++ R/libbi.R
@@ -230,11 +230,6 @@ run.libbi <- function(x, client, proposal=c("model", "prior"), options, config, 
       x$.cache$data <- list()
       x$.cache$thin <- integer(0)
 
-      if ("end-time" %in% names(all_options) && !("noutputs" %in% names(all_options))) {
-        runtime <- all_options[["end-time"]] - ifelse("start-time" %in% names(all_options), all_options[["start-time"]], 0)
-        warning("No 'noputputs' given; will set to 'end-time' - 'start-time' (", runtime, ")")
-        all_options[["noutputs"]] <- runtime
-      }
       if (!("output-file" %in% names(all_options))) {
         x$output_file_name <- tempfile(pattern=paste(x$model$name, "output", sep = "_"),
                                        fileext=".nc",
