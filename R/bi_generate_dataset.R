90c91d02f7e40f26b82b3ae2c22ea4ceb26863d9
sbfnk <sbfnk@users.noreply.github.com>
2017-01-18 21:33:18 +0000

    update the way bi_generate_dataset calls libbi

--- R/bi_generate_dataset.R
+++ R/bi_generate_dataset.R
@@ -7,14 +7,10 @@
 #' (see \code{\link{run}}, otherwise they are generated from the prior specified
 #' in the model. The end time should be specified using the "end_time" option. If this is not given,
 #' only a parameter set is sampled. 
-#' @param ... arguments to be passed to \code{\link{libbi}} (with run = TRUE), especially 'end_time'
+#' @param ... arguments to be passed to \code{\link{libbi}} and \code{\link{sample}}, especially 'model' and 'end_time'
 #' @return generated data set
 #' @export
 bi_generate_dataset <- function(...){
-  ## if (missing(endtime)){
-  ##   stop("please specify the final time index!")
-  ## }
-
   dot_options <- list(...)
   if ("options" %in% dot_options) {
     options <- dot_options[["options"]]
@@ -30,11 +26,8 @@ bi_generate_dataset <- function(...){
   options[["target"]] <- "joint"
   options[["nsamples"]] <- 1
 
-  libbi_new_options <-
-    c(list(client = "sample", options = options, run = TRUE),
-      dot_options)
-
-  bi_object <- do.call(libbi$new, libbi_new_options)
+  bi_object <- do.call(libbi, c(options, dot_options))
+  bi_object <- sample(bi_object)
 
   return(bi_object)
 }
