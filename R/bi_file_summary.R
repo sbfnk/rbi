b9c74e024a848210533fcfb826e4ae79e16cfa8c
sbfnk <sbfnk@users.noreply.github.com>
2017-01-27 08:16:39 +0000

    bi_file_summary: work on libbi objects

--- R/bi_file_summary.R
+++ R/bi_file_summary.R
@@ -5,13 +5,19 @@
 #' This function prints a little summary of the content
 #' of a NetCDF file, as well as its creation time. You can
 #' then retrieve variables of interest using \code{\link{bi_read}}.
-#' @param filename path to a NetCDF file
+#' @param x A \code{\link{libbi}} object (in which case the output file will be taken) or a path to a NetCDF file
 #' @return None
 #' @export
 #' @importFrom ncdf4 nc_open nc_close
 #' 
-bi_file_summary <- function(filename){
-  cat("Summary of file", filename, "\n")
+bi_file_summary <- function(x){
+  if ("libbi" %in% class(x)) {
+    assert_output(x)
+    filename <- x$output_file_name
+    message("Summary of libbi output file")
+  } else {
+    filename <- x
+  }
   print(file.info(tools::file_path_as_absolute(filename))[,c("mtime")])
   ncfile <- nc_open(tools::file_path_as_absolute(filename), verbose = FALSE)
   print(ncfile)
