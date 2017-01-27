95a45d173f29e543c236429d21fcb144bf0285a7
sbfnk <sbfnk@users.noreply.github.com>
2017-01-27 10:51:39 +0000

    bi_generate_dataset: seed option

--- R/bi_generate_dataset.R
+++ R/bi_generate_dataset.R
@@ -7,10 +7,11 @@
 #' (see \code{\link{run}}, otherwise they are generated from the prior specified
 #' in the model. The end time should be specified using the "end_time" option. If this is not given,
 #' only a parameter set is sampled. 
+#' @param seed random seed; see the seed option of \code{\link{sample}} for details
 #' @param ... arguments to be passed to \code{\link{libbi}} and \code{\link{sample}}, especially 'model' and 'end_time'
 #' @return generated data set
 #' @export
-bi_generate_dataset <- function(...){
+bi_generate_dataset <- function(seed, ...){
   dot_options <- list(...)
   if ("options" %in% dot_options) {
     options <- dot_options[["options"]]
@@ -26,8 +27,11 @@ bi_generate_dataset <- function(...){
   options[["target"]] <- "joint"
   options[["nsamples"]] <- 1
 
+  sample_options <- list()
+  if (!missing(seed)) sample_options[["seed"]] <- seed
+
   bi_object <- do.call(libbi, c(options, dot_options))
-  bi_object <- sample(bi_object)
+  bi_object <- do.call(sample, c(list(x=bi_object), sample_options))
 
   return(bi_object)
 }
