f4a8b98db8b7ab272c258ad5428054025a5d5187
sbfnk <sebastian.funk@lshtm.ac.uk>
2017-02-05 23:32:45 +0000

    read_var_input: remove comments

--- R/read_var_input.R
+++ R/read_var_input.R
@@ -10,10 +10,6 @@
 ##' @author Lawrence Murray, \email{lawrence.murray@@csiro.au}
 ##' @importFrom ncdf4 ncvar_get
 read_var_input <- function(nc, name, coord, ps, ts, ...){
-  # check arguments
-  # if nargin < 2 || nargin > 5
-  # print_usage ();
-  # end
   if (!is.character (name)){
     stop('name must be a string')
   }
@@ -26,23 +22,6 @@ read_var_input <- function(nc, name, coord, ps, ts, ...){
   if (missing(ts)){
     ts <- c()
   }
-  # check dimensions
-#   if (nc_var_has_dim (nc, name, 'np')){
-#     P = length(nc$dim[["np"]]$vals)
-#   } else {
-#     P = 1
-#   }
-#   if (length(ps) == 0){
-#     ps = 1:P
-#   }
-#   if (nc_var_has_dim (nc, name, 'nr')){
-#     T = length(nc$dim[["nr"]]$vals);
-#   } else {
-#     T = 1
-#   }
-#   if (length(ts) == 0){
-#     ts = 1:T
-#   }
   # read
   values <- ncvar_get(nc, name, ...)
   if (is.null(dim(values))){
