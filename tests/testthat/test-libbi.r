55cc5d346b3ca586a61516fa302c97f88fdfadf1
BlackEdder <edder@tkwsping.nl>
2017-02-09 09:41:54 +0000

    Add test for multi dimensional model

--- tests/testthat/test-libbi.r
+++ tests/testthat/test-libbi.r
@@ -31,3 +31,39 @@ test_that("we can rewrite a model",
   skip_on_cran()
   rewrite(bi)
 })
+
+test_that("we can run libbi on stratified model",
+{
+  skip_on_cran()
+  model_str <- "
+model stratified {
+  const no_age = 2
+  dim age(no_age)
+
+  obs M[age]
+
+  state N[age] (has_input = 0)
+  state e
+
+  sub parameter {
+  }
+  
+  sub initial {
+    N[age] <- 1
+  }
+  
+  sub transition {
+    e ~ gaussian(mean = 0, std = 1)
+    N[age] <- N[age] + e
+  }
+  
+  sub observation {
+    M[age] ~ gaussian(mean = N[age], std = 10)
+  }
+}
+  "
+  bi <- bi_model(lines = stringi::stri_split_lines(model_str)[[1]])
+  dataset <- bi_generate_dataset(bi, end_time=50)
+  bi <- sample(bi, sample_obs=TRUE, obs=dataset, fix=c(dummy=0))
+  expect_equal(ncol(bi_read(bi)$M), 3)
+})
