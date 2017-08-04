43684f2011470eeb514851d4413890c0cdc046b3
sbfnk <sebastian.funk@lshtm.ac.uk>
2017-08-04 09:33:12 +0100

    remove warning

--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("The user interface of 'RBi' has changed fundamentally with version 0.7.0, and scripts based on previous versions will probably have to be modified to work. Refer to the introductory vignette and the user manual for the new syntax.")
-}
