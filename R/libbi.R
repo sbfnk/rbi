fec8a0435122bcc8e92257b06cbc186274f8dab7
sbfnk <sebastian.funk@lshtm.ac.uk>
2018-01-23 14:32:30 +0000

    if predicting, remove proposals

--- R/libbi.R
+++ R/libbi.R
@@ -373,9 +373,11 @@ run.libbi <-  function(x, client, proposal=c("model", "prior"), model, fix, opti
     if ("target" %in% names(all_options) &&
         all_options[["target"]] == "prediction") {
       run_model <- remove_lines(run_model, "parameter")
+      run_model <- remove_lines(run_model, "proposal_parameter")
       if ("init-file" %in% names(x$option)) {
         init_given <- bi_contents(x$options[["init-file"]])
         run_model <- remove_lines(run_model, "initial", only = init_given)
+        run_model <- remove_lines(run_model, "proposal_initial")
       }
       run_model_modified <- TRUE
     }
