bedb3974a9299be5cb78f1f3473506cdf73d0517
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2018-12-07 12:20:12 +0000

    document flatten

--- R/flatten.r
+++ R/flatten.r
@@ -1,13 +1,27 @@
-#' @importFrom data.table data.table setDF rbindlist :=
+#' @rdname flatten
+#' @name flatten
+#' @title Flatten list of data frames
+#' This function takes a list of data frames (such as, for example, returned by \code{\link{bi_read}}) and converts it to a flat data frame
 #' @export
+#' @param x The list of data frames
 flatten <- function(x) {
   res <- lapply(names(x), function(y) {
     if (is.data.frame(x[[y]])) {
-      data.table::data.table(x[[y]])[, var := y]
+      x[[y]]$var <- y
     } else {
-      data.table::data.table(value=x[[y]])[, var := y]
+      x[[y]] <- data.frame(value=x[[y]], var=y)
     }
+    x[[y]]
   })
+
   res <- data.table::setDF(data.table::rbindlist(res, fill=TRUE))
-  return(res)
+  early_columns <- "np"
+  late_columns <- c("time", "value", "var")
+  columns <- colnames(res)
+  reorder <-
+      c(intersect(early_columns, columns),
+        setdiff(columns, union(early_columns, late_columns)),
+        intersect(late_columns, columns))
+
+  return(res[, reorder])
 }
